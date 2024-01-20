9afa6f006405fd922323f7187658dfbb222a3598
Fairies-Feast (Fairies0Feast) <86023563+Fairies-Feast@users.noreply.github.com>
2024-01-20 17:23:21 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,19 @@
 # To get this, pop open a BASH terminal and type in the command below:
 #curl https://amsilla.github.io/install.sh >> install.sh && bash install.sh
 echo "The Plate installer has been installed."
-unalias plate
-curl https://amsilla.github.io/plate.sh >> plate.sh # > ls || plate.sh
+echo "Please type the below command into the terminal"
+echo "-------------------"
+echo "unalias plate"
+echo "-------------------"
+echo "If it says that the plate alias is not found, it does not matter"
+echo "Press enter to begin the installation"
+read
+curl https://amsilla.github.io/plate.sh >> plate.sh
 chmod +x plate.sh
-alias plate='./plate.sh'
-rm install.sh
-echo "Plate has been installed and will run by the command 'plate'"
+echo "One last step. Please type the below command into the terminal."
+echo "-----------------------"
+echo "alias plate='./plate.sh'"
+echo "------------------------"
+echo "Press enter to continue"
+read
+echo "The installation has finished!"
